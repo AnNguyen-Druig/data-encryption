b246dc21f6dfec45ba38f9d6abb67c41d4938801
Devteria <devteriadotcom@gmail.com>
2025-11-23 14:53:22 +0700

    Initial commit

---
+++ src/main/java/com/devteria/data_encryption_demo/DataEncryptionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devteria.data_encryption_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataEncryptionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataEncryptionDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devteria/data_encryption_demo/DataEncryptionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devteria.data_encryption_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataEncryptionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
